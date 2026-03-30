1e5fb2e77e517c41ea63933406e09c1ec8ab2f4c
Enoxboo <matteo.marquant@ynov.com>
2026-03-30 16:10:22 +0200

    feat: add tournament routes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import tournamentRoutes from './src/routes/tournament.routes.js'
+
 const express = require('express')
 const dotenv = require('dotenv')
 
@@ -17,7 +19,7 @@ app.get('/health', (req, res) => {
 // TODO: importer les routes ici au fur et à mesure
 // app.use('/api/players', require('./routes/players.routes'))
 // app.use('/api/games', require('./routes/games.routes'))
-// app.use('/api/tournaments', require('./routes/tournaments.routes'))
+app.use('/tournaments', tournamentRoutes)
 
 // Handler 404
 app.use((req, res) => {
